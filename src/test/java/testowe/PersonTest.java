1eff8cf4afd41f58960f580cf50ff62244e540f0
kasia <kgbsz@wp.pl>
2019-03-20 20:30:53 +0100

    testy1
    zmiany kolejne
     zajecia2

--- src/test/java/testowe/PersonTest.java
+++ src/test/java/testowe/PersonTest.java
@@ -3,6 +3,7 @@ package testowe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -11,11 +12,12 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
-
+// testow sie nie dokumentuje
+// nazwa testu ma mowic od czego jest , i sprawdza czy kod jest prawidlowy
 public class PersonTest {
 
-     Person person = new Person("Jan Kowalski");
-     Person spouse = new Person("Alicja Nowak");
+    Person person = new Person("Jan Kowalski");
+    Person spouse = new Person("Alicja Nowak");
 
 
     @Test
@@ -55,7 +57,7 @@ public class PersonTest {
     }
 
     @Test
-    void loverShouldNotMarriageMarriedPerson(){  // kochanek nie powiennien wziasc slubu z osoba po slubie
+    void loverShouldNotMarriageMarriedPerson() {  // kochanek nie powiennien wziasc slubu z osoba po slubie
 
         Person lover = new Person(" Bartek K ");
 
@@ -63,15 +65,17 @@ public class PersonTest {
         lover.marriage(person);
         // sprawdzamy
         assertAll(
-                () -> Assertions.assertNull(lover.getSpouse(), lover.getName()+
-                        "jest  w zwiazku z" ),
+                () -> Assertions.assertNull(lover.getSpouse(), lover.getName() +
+                        "jest  w zwiazku z"),
                 () -> Assertions.assertEquals(spouse, person.getSpouse())
         );
-      //  assertNull(lover.getSpouse());
-      //  assertEquals(spouse, person.getSpouse());  // sprawdzamy ,jan kowlaski za zone powinnien miec ciagle alicje nowak
+        //  assertNull(lover.getSpouse());
+        //  assertEquals(spouse, person.getSpouse());  // sprawdzamy ,jan kowlaski za zone powinnien miec ciagle alicje nowak
     }
-    @Test // czy osoby sa po slubie ze soba
-    void personShouldNotBeMarriedWithAnotherOne(){
+
+    @Test
+        // czy osoby sa po slubie ze soba
+    void personShouldNotBeMarriedWithAnotherOne() {
 
 
         person.marriage(spouse);
@@ -79,8 +83,9 @@ public class PersonTest {
         Assertions.assertFalse(person.isMarriedWithAnotherPerson(spouse));   // sprawdzamy czy wszystko w ziwazku jest ok?
     }
 
-    @Test   // czy ta 3 osoba kogos nie ma
-    void personShouldBeMarriedWithAnotherOne(){
+    @Test
+        // czy ta 3 osoba kogos nie ma
+    void personShouldBeMarriedWithAnotherOne() {
 
         Person lover = new Person(" Bartek K ");
 
@@ -90,7 +95,7 @@ public class PersonTest {
     }
 
     @Test
-    void personShouldNotHaveSpouseAfterDivorce(){
+    void personShouldNotHaveSpouseAfterDivorce() {
 
         person.marriage(spouse);
         person.divorce();
@@ -101,8 +106,8 @@ public class PersonTest {
     @Test
     void spouseShouldNotHaveSpouseAfterDivorce() {
         // Arrange // Given
-    //    testowe.Person person = new testowe.Person("Jan Kowalski");
-    //    testowe.Person spouse = new testowe.Person("Alicja Nowak");
+        //    testowe.Person person = new testowe.Person("Jan Kowalski");
+        //    testowe.Person spouse = new testowe.Person("Alicja Nowak");
         // Act // When
         person.marriage(spouse);
         person.divorce();
@@ -112,7 +117,7 @@ public class PersonTest {
     }
 
     @Test
-    void personShouldEarnSomeMoney(){  // zarabia pieniadze pierwszy raz
+    void personShouldEarnSomeMoney() {  // zarabia pieniadze pierwszy raz
         person.earn(1000);
 
         assertEquals(1000, person.getMoney());
@@ -120,75 +125,81 @@ public class PersonTest {
     }
 
     @Test
-    void personShouldEarnMuchMoney(){  // zarabia pieniadze kolejny raz
+    void personShouldEarnMuchMoney() {  // zarabia pieniadze kolejny raz
         person.earn(1000);
         person.earn(500);
 
-        assertEquals(1500,person.getMoney());
+        assertEquals(1500, person.getMoney());
     }
-        // sprawdzenie -- wyjatek
+    // sprawdzenie -- wyjatek
 
-        @Test
-    void personShouldNotEarnMinusMoney(){
+    @Test
+    void personShouldNotEarnMinusMoney() {
         person.earn(100);
-         Exception exception = assertThrows(MinusMoneyException.class,() -> {
+        Exception exception = assertThrows(MinusMoneyException.class, () -> {
             person.earn(-100);
         });
         assertEquals("No minus money", exception.getMessage()); // zarobki nie moga byc ujemne
-        }
+    }
 
-        @Test
-    void personShouldEarnMoneyAfterWork(){
-       assertTimeout(Duration.ofMillis(1000), () -> {     // assertTimeoutPreemptively- jesli wykonuje sie za dlugo to konczy,ucina
-           person.work(100, 300);          //zwykly assertTimeout metoda czeka do zakonczenia wykonywania metody, i metoda timeout powie o ile czasu za dlugo wykonywala sie metoda,
-       });
+    @Test
+    void personShouldEarnMoneyAfterWork() {
+        assertTimeout(Duration.ofMillis(1000), () -> {     // assertTimeoutPreemptively- jesli wykonuje sie za dlugo to konczy,ucina
+            person.work(100, 300);          //zwykly assertTimeout metoda czeka do zakonczenia wykonywania metody, i metoda timeout powie o ile czasu za dlugo wykonywala sie metoda,
+        });
         // sprawdzamy jak dlugo bedzie wykonywana metoda przy wiekszej sumie
         assertEquals(300, person.getMoney());
-        }
+    }
 
-        @Test
+    @Test
         // uzywamy gdy chcemy dac warunki
-        void testSomethingWithAssume(){
+    void testSomethingWithAssume() {
         System.setProperty("ENV", "PROD");
         // test moze sie wykonac dopiero w srodowisku produkcyjnym
         assumeTrue(System.getProperty("ENV").equals("PROD"));
         // ALT + ENTER - import //
         assertTrue(true);
-        }
+    }
 
-        // testy sprawdzajace - mozna usunac -zrobic refaktor - testy parametryzowane sprawdzja to samo
+    // testy sprawdzajace - mozna usunac -zrobic refaktor - testy parametryzowane sprawdzja to samo
     @Test
-    void emailShouldBeValid1(){
+    void emailShouldBeValid1() {
         person.setEmail("jan.kowalski@szkolenie.pl");
 
         assertTrue(person.isEmailValid());  // powinno zwrocic true
     }
+
     @Test
-    void emailShouldBeValid2(){
+    void emailShouldBeValid2() {
         person.setEmail("jan@szkolenie.pl");
 
         assertTrue(person.isEmailValid());  // powinno zwrocic true
     }
+
     @Test
-    void emailShouldBeValid3(){
+    void emailShouldBeValid3() {
         person.setEmail("jaszkolenie.pl");
 
         assertFalse(person.isEmailValid());  // powinno zwrocic false
     }
 
 
-    @ParameterizedTest  // przekazujemy tylko prawidlowe testy , // testy poprzednie emaili sa zbedne,mozna usunac,bo ten tez sprawdza emaile
-    @ValueSource(strings = {"jan.kowalski@szkolenie.pl","jan@szkolenie.pl","jakie@szkolenie.pl","@jakszolenie.pl" })  // kazda wartosc bedzie wpadala jako argument email, test zostanie uruchomiony tyle razy ile jest przypadkow
-    void emailsShouldBeValid(String email){
+    @ParameterizedTest
+    // przekazujemy tylko prawidlowe testy , // testy poprzednie emaili sa zbedne,mozna usunac,bo ten tez sprawdza emaile
+    @ValueSource(strings = {"jan.kowalski@szkolenie.pl", "jan@szkolenie.pl", "jakie@szkolenie.pl", "@jakszolenie.pl"})
+        // kazda wartosc bedzie wpadala jako argument email, test zostanie uruchomiony tyle razy ile jest przypadkow
+    void emailsShouldBeValid(String email) {
         person.setEmail(email);
 
         assertTrue(person.isEmailValid());
 
     }
 
-    @ParameterizedTest  // przekazujemy tylko prawidlowe testy , // testy poprzednie emaili sa zbedne,mozna usunac,bo ten tez sprawdza emaile
-    @ValueSource(strings = {"@szkolenie.pl","jan.I.szkolenie.pl","@szkolenie.pl","@jakszolenie.pl" })  // kazda wartosc bedzie wpadala jako argument email, test zostanie uruchomiony tyle razy ile jest przypadkow
-    void emailsShouldNotBeValid(String email){
+    @ParameterizedTest
+    // przekazujemy tylko prawidlowe testy , // testy poprzednie emaili sa zbedne,mozna usunac,bo ten tez sprawdza emaile
+    @ValueSource(strings = {"@szkolenie.pl", "jan.I.szkolenie.pl", "@szkolenie.pl", "@jakszolenie.pl"})
+        // kazda wartosc bedzie wpadala jako argument email, test zostanie uruchomiony tyle razy ile jest przypadkow
+    void emailsShouldNotBeValid(String email) {
         person.setEmail(email);
 
         assertFalse(person.isEmailValid());
@@ -197,20 +208,44 @@ public class PersonTest {
 
     // test parametryzowany z metoda fabrykujaca
     @ParameterizedTest
-    @MethodSource(value = "provideValidEmails" )
-    void emailsShouldByValidByMethodSource(String email){
+    @MethodSource(value = "provideValidEmails")
+    void emailsShouldByValidByMethodSource(String email) {
         person.setEmail(email);
 
         assertTrue(person.isEmailValid());   // ma zwrocic true
     }
+
+
     // oczekiwanie co zwroci,co ma sprawdzic
-    static Stream provideValidEmails(){
-        return Stream.of("jan.kowalski@szkolenie.pl","jan@szkolenie.pl","jakie@szkolenie.pl");
+    static Stream provideValidEmails() {
+        return Stream.of("jan.kowalski@szkolenie.pl", "jan@szkolenie.pl", "jakie@szkolenie.pl");
     }
 
 
+    @ParameterizedTest
+    @MethodSource(value = "provideEmails")
+    void checkEmailCorrections(String email, boolean expectedValidation) {
+        person.setEmail(email);
 
+        assertEquals(expectedValidation, person.isEmailValid(), "Email " + email + "should be : " + expectedValidation);
     }
+
+
+    // provide - dostarcza cos poprostu, - ,    ale w metodach abstarakcyjnych jest wykorzystywane w wzorcach projektowych
+    //  zwraca streama i przechodzi po streamie i jest wykonywany z kazdym argumentem i jest wykonywnay dla kazdego testu z osobna
+    static Stream provideEmails() {
+        return Stream.of(
+                Arguments.of("jan.kowalski@szkolenie.pl", true),
+        Arguments.of("jan@szkolenie.pl", true),
+        Arguments.of("jakie@szkolenie.pl", false)
+        );
+    }
+
+
+    
+
+}
+
 
 
 
