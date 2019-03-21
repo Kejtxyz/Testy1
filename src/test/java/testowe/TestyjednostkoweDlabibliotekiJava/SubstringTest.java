4ad46feaa9df801edeba3262fdf2313cc5703db6
kasia <kgbsz@wp.pl>
2019-03-21 18:58:34 +0100

    testy1 testy jednostkowe Testy substring Java zmiany 4 zajecia3

--- src/test/java/testowe/TestyjednostkoweDlabibliotekiJava/SubstringTest.java
+++ src/test/java/testowe/TestyjednostkoweDlabibliotekiJava/SubstringTest.java
@@ -1,5 +1,27 @@
 package testowe.TestyjednostkoweDlabibliotekiJava;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SubstringTest {
-    
+
+    String string = "Dzban";
+
+
+    @Test
+    void shouldReturnFirstCharacter(){
+     // stworzyc substring
+     String result =  string.substring(0, 1);
+
+     assertEquals("D", result);
+     assertThat(result)
+             .isEqualTo("D")
+             .isEqualToIgnoringCase("d")
+             .hasSize(1);
+
+
+    }
+
 }
