47e6142cc661ef6bf6f8b8df9041848c719b4800
kasia <kgbsz@wp.pl>
2019-03-23 11:06:48 +0100

    testy3 stub zmiany 6 zajecia4

--- src/test/java/testowe/messenger/MessengerTest.java
+++ src/test/java/testowe/messenger/MessengerTest.java
@@ -2,6 +2,7 @@ package testowe.messenger;
 
 import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
 import messenger.Messenger;
+import messenger.TemplateEngine;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
@@ -10,8 +11,11 @@ public class MessengerTest {
 
     @Test
     void shouldSendMessenger(){
-        // dummy //
+        //  dummy //
         Template templateMock = mock(Template.class);
+        //  stub  //
+        TemplateEngine templateEngine = mock(TemplateEngine.class);
+
         Messenger messenger = new Messenger();
 
         messenger.sendMessage();
